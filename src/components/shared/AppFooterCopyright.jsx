a0da463c9f789796ce04de159620f585bba4dcdd
MISHU37201 <147590773+MISHU37201@users.noreply.github.com>
2025-06-21 23:10:20 +0500

    added name,contact number, email id in footer

--- src/components/shared/AppFooterCopyright.jsx
+++ src/components/shared/AppFooterCopyright.jsx
@@ -2,22 +2,17 @@ function AppFooterCopyright() {
 	return (
 		<div className="font-general-regular flex justify-center items-center text-center">
 			<div className="text-lg text-ternary-dark dark:text-ternary-light">
-				&copy; {new Date().getFullYear()}
-				<a
-					href="https://github.com/realstoman/react-tailwindcss-portfolio"
-					target="__blank"
-					className="hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
-				>
-					React & Tailwind CSS Portfolio
-				</a>
-				.
-				<a
-					href="https://stoman.me"
-					target="__blank"
-					className="text-secondary-dark dark:text-secondary-light font-medium uppercase hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
-				>
-					Stoman
-				</a>
+				
+				<p>
+					{`<Rimsha />`}
+				</p>
+				<p>
+					03215835886,
+					03481722257
+				</p>
+				<p>
+					rimshasaqlain08@gmail.com
+				</p>
 			</div>
 		</div>
 	);
